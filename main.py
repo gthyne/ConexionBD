8ea305c7fe48fb6698c80aa4a0f5f177458840a5
GarvonT <garvont@yahoo.com>
2024-03-23 10:03:54 -0600

    Cambios

--- main.py
+++ main.py
@@ -3,43 +3,50 @@ import daoConnection as dao
 import clases as c
 
 os.system('cls')
-conex = dao.Connection("localhost", "root", "", "dbregisters")
+conex = dao.Connection("localhost", "root", "", "registrodb")
 conex.connect()
 
-def insertarciudad():
-    nombre = input("nombre ciudad")
-    ciudad = c.city(nombre, 1)
-    daoCity = dao.daocity(conex)
+def insertarCiudad():
+    nombre = input("nombre ciudad ")
+    ciudad = c.City(nombre, 1)
+    daoCity = dao.DaoCity(conex)
+    daoCity.insert(ciudad)
+
+def mostrar():
+    daoCity = dao.DaoCity(conex)
     cities = daoCity.get_all()
     for city in cities:
         print (city)
     #insertar
-    daoCity.insert(ciudad)
+    
 
-    def Menu():
-        print("1,insertar ciudad")
-        print("2,editar ciudad")
-        print("3,mostrar ciudad")
-        print("4,Eliminar ciudad")
-        print("5,buscar  ciudad")
-        print("6,salir")
+def menu():
+    print("1,insertar ciudad")
+    print("2,editar ciudad")
+    print("3,mostrar ciudad")
+    print("4,Eliminar ciudad")
+    print("5,buscar  ciudad")
+    print("6,salir")
 
 
-    def main():
-        op = 0
-        while(op !=6):
-            menu()
-        opcion = int(input("dime tu opcion")) 
+def main():
+    op = 0
+    while(op !=6):
+        menu()
+        opcion = int(input("dime tu opcion ")) 
         if (opcion == 1):
-            insertarciudad()
-            os.system("pausa")
+            insertarCiudad()
+            os.system("pause")
+        elif(opcion==3):
+            mostrar()
+            os.system("pause")
+
+main()
 
 
 
 
-
-
-
+"""
 
 
 #instanciar modelo
@@ -66,3 +73,4 @@ daoCity.insert(city6)
 cities = daoCity.get_all()
 for city in cities:
     print(city)
+"""
